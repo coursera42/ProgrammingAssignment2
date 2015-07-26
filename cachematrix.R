1aa3d31e338934fde683744c3b995d61c6700496
Alexander Burris <alexander@burris.de>
2015-07-26 17:35:08 +0200

    fin

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this cached solve(inversing) for matrices.
+## this is programming assignment no2 of https://class.coursera.org/rprog-030/human_grading/view/courses/975104/assessments/3/submissions
 
-## Write a short comment describing this function
 
+## create a inverematrix function with getter&setter
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## returns a inversed matrix, caching the results to speed up multiple calls on the same matrix.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    
+    i
 }
